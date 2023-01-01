6542d523da8f2610877ed21f2753841042475c3c
touhf <tobytubar@gmail.com>
2023-01-01 13:05:32 +0500

    fixed urls, added new test post

--- django_project/urls.py
+++ django_project/urls.py
@@ -13,13 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from . import views
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
 ]
