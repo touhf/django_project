896ba6f96e200715d13f568509bcaffb3f832154
touhf <tobytubar@gmail.com>
2023-01-03 19:47:41 +0500

    better way to upload pictures

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 from . import views
 from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.post_detail, name='post_detail'), #deleted the .as_view() part
     path("about", views.AboutPageView.as_view(), name="about")
 ]
+
+urlpatterns += staticfiles_urlpatterns()
